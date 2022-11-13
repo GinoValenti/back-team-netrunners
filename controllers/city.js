cda3318bcf4ce50c9139f9e5c7fb2bdb31cdfe34
GastonZ <gastonzappullaryu@gmail.com>
2022-11-12 21:52:29 -0300

    MT-B1-17

--- controllers/city.js
+++ controllers/city.js
@@ -53,6 +53,36 @@ const controller = {
         message: error.message,
       });
     }
+  },
+
+  update: async(req,res) => {
+
+    let { id } = req.params
+ 
+    try {
+        let one = await City.findOneAndUpdate({ _id: id }, req.body, {new: true})
+        if (one) {
+          res.status(200).json({
+            id: one._id,
+            success: true,
+            message: "A city has been modified",
+          })
+        } else {
+          res.status(404).json({
+            success: false,
+            message: "City wasn't found",
+          })
+        }
+
+    } catch (error) {
+        res.status(400).json({
+          success: false,
+          message: error.message
+        })
+    }
   }
 }
+
+
+
 module.exports = controller;
