46a8827694e8a814a840eace0c5c634c25a4c4e9
GastonZ <gastonzappullaryu@gmail.com>
2022-11-14 17:49:10 -0300

    MT-B1-31

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -30,6 +30,49 @@ const controller = {
           });
         }
       },
+      create: async (requerimiento, respuesta) => {
+        try {
+          let new_itinerary = await Itinerary.create(requerimiento.body);
+    
+          respuesta.status(201).json({
+            id : new_itinerary._id,
+            success: true,
+            message: "El itinerary se creo correctamente",
+          });
+        } catch (error) {
+          console.log(error.message);
+          respuesta.status(400).json({
+            success: false,
+            message: error.message,
+          });
+        }
+      },
+      update: async (req,res) => {
+
+        let { id } = req.params
+     
+        try {
+            let one = await City.findOneAndUpdate({ _id: id }, req.body, {new: true})
+            if (one) {
+              res.status(200).json({
+                id: one._id,
+                success: true,
+                message: "A itinerary has been modified",
+              })
+            } else {
+              res.status(404).json({
+                success: false,
+                message: "Itinerary wasn't found",
+              })
+            }
+    
+        } catch (error) {
+            res.status(400).json({
+              success: false,
+              message: error.message
+            })
+        }
+      },
 }
 
 module.exports = controller;
