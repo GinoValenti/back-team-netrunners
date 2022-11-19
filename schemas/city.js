e7486ab25f11689dced57b83ba48c9c1791ec180
GastonZ <gastonzappullaryu@gmail.com>
2022-11-18 21:04:41 -0300

    MT-01-37

--- schemas/city.js
+++ schemas/city.js
@@ -1,9 +1,25 @@
 const joi = require('joi')
 
 const schema = joi.object({
-    title: joi.string().required() ,
-    continent: joi.string().required(),
-    image: joi.string().required() ,
-    population: joi.number().required() ,
-    userId: joi.any().required() 
+    title: joi
+        .string()
+        .required()
+        .min(5)
+        .max(20),
+    continent: joi
+        .string()
+        .required(),
+    image: joi
+        .string()
+        .required() ,
+    population: joi
+        .number()
+        .required() ,
+    userId: joi
+        .string()
+        .required()
+        .min(24)
+        .max(24) 
 })
+
+module.exports = schema
