722006eda17d15fa725803157cb0a6d0b5c28179
GinoValenti <ginorvalenti@gmail.com>
2022-11-24 14:41:10 -0300

    doing sign in back

--- models/User.js
+++ models/User.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose")
 const schema = new mongoose.Schema({
     name: {type: String, required: true},
     lastName: {type: String, required: true},
+    role:{type:String,required:true},
     age: {type: Number, required: true},
+    photo:{type:String,required:true},
     email:{type: String, required: true},
     password:{type: String, required: true},
     code:{type: String, required: true},
-    verified:{type: Boolean, required: true},
-    logged:{type: Boolean, required: true},
+    verified:{type: Boolean},
+    logged:{type: Boolean},
     
 })
 const User = mongoose.model("user", schema)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
 let router = require('express').Router()
+const accountExistsSignIn = require('../middlewares/accountExistsSignIn')
+const mustSignIn = require('../middlewares/mustSignIn')
+const { ingresar, ingresarConToken, salir } = require('../controllers/user')
+const accountHasBeenVerified = require('../middlewares/accountHasBeenVerified')
+const passport = require('../config/passport')
+const schema = require("../schemas/userLogin")
+const validator = require("../middlewares/validator")
 
-let {create} = require('../controllers/admin')
 
-router.post("/users", create);
-
+router.post('/signin',validator(schema), accountExistsSignIn,accountHasBeenVerified,ingresar)
+router.post('/token', passport.authenticate('jwt', {session: false}), mustSignIn, ingresarConToken)
+router.put('/signout', passport.authenticate('jwt', {session: false}), salir)
 module.exports = router
