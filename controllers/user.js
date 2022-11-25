6b7b80bcbb1260280ead9f7764183df7496fceac
GinoValenti <ginorvalenti@gmail.com>
2022-11-25 15:39:21 -0300

    doing back for sign in

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const Usuario = require('../models/User')
-const bcryptjs = require('bcryptjs') //de esta libreria vamos a utilizar el método hashSync para encriptar la contraseña
-const crypto = require('crypto')//de este modulo vamos a requerir el método randomBytes
+const bcryptjs = require('bcryptjs') 
+const crypto = require('crypto')
 const accountVerificationEmail = require('./accountVerificationEmail')
 const { invalidCredentialsResponse, userSignedOutResponse } = require('../config/responses')
 const jwt = require('jsonwebtoken')
@@ -11,13 +11,7 @@ const controller = {
         const { password } = req.body;
         const {user} = req;
 
-        //método para que un usuario inicie sesión
-        //luego de pasar por todas las validaciones:
-            //desestructura la contraseña y el objeto user que vienen en el req
-            //compara las contraseñas
-                //si tiene éxito debe generar y retornar un token y debe redirigir a alguna página (home, welcome)
-                    //además debe cambiar online de false a true
-                //si no tiene éxito debe responder con el error
+
         try {
             const verificarContraseña = bcryptjs.compareSync(password, user.password)
 
@@ -44,17 +38,15 @@ const controller = {
     },
     ingresarConToken: async (req, res, next) => {
 
-        //método para que un usuario que ya inicio sesión no la pierda al recargar
-        //solo para usuarios registrados en nuestra app (social loguin se maneja en front)
-        //luego de pasar por todas las validaciones:
-        //debe responder con los datos del usuario
+    
         let {user} = req
         try {
             return res.json({
                 response: {
                     user: {
                         name: user.name,
-                        photo: user.photo
+                        photo: user.photo,
+                        role: user.role
                     },
                 },
                 success: true,
@@ -66,10 +58,7 @@ const controller = {
     },
     
     salir: async (req, res, next) => {
-        //método para que un usuario cierre sesión (cambia online de true a false)
-        //solo para usuarios registrados en nuestra app (social logout se maneja en front)
-        //si tiene éxito debe cambiar online de true a false
-        //si no tiene éxito debe responder con el error
+      
         const { id } = req.user
         
         try {
--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,16 +1,15 @@
-const bcryptjs = require('bcryptjs') // de este modulo vamos a requerir el metodo randomBytes
+const bcryptjs = require('bcryptjs') 
 const crypto = require('crypto')
 const User = require('../models/User')
 const accountVerificationEmail = require('./accountVerificationEmail')
 const { userSignedUpResponse, userNotFoundResponse, invalidCredentialsResponse, userSignedOutResponse } = require('../config/responses')
-// Aqui deberia requerir las funciones de respuesta para luego usar en linea 26
+
 
 const controller = {
     
     register : async(req,res,next) => {
 
-        //metodo para que un usuario se registre
-        //luego de pasar por todas las validaciones
+ 
 
         let { name, lastname, age, email, password, photo, role } = req.body
 
@@ -18,12 +17,12 @@ const controller = {
         let logged = false
         let code = crypto.randomBytes(10).toString('hex')
 
-        password = bcryptjs.hashSync(password,10) //encripto o hasheo la constraseña
+        password = bcryptjs.hashSync(password,10) 
 
         try {
 
             await User.create({ name, lastname, age, email, password, photo , role, verified, logged, code })
-            // envia mail de verficacion (con transportador)
+      
             await accountVerificationEmail(email, code)
             return userSignedUpResponse(req, res)
 
