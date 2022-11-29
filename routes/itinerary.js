e0323c11e8f6f5623ae3c844902489d465d74487
GastonZ <gastonzappullaryu@gmail.com>
2022-11-29 12:16:01 -0300

    fixing passport

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,12 +1,13 @@
 let router = require('express').Router()
+const passport = require('../config/passport')
 
 const { readOne, create, update, destroy, read } = require('../controllers/itinerary')
 
 router.get('/', read)
 router.get("/itinerary", readOne)
-router.post("/", create)
-router.put("/itineraryUpdate/:id", update)
-router.delete("/itineraryDelete/:id", destroy)
+router.post("/", passport.authenticate('jwt', {session: false}), create)
+router.put("/itineraryUpdate/:id", passport.authenticate('jwt', {session: false}), update)
+router.delete("/itineraryDelete/:id", passport.authenticate('jwt', {session:false}), destroy)
 
 
 module.exports = router
