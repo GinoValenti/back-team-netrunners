a2cd0f484a47e07eafdefe435de46904ddb7f1bd
GinoValenti <ginorvalenti@gmail.com>
2022-12-01 22:21:36 -0300

    fixing

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,12 @@
 let router = require("express").Router()
 let {read,create,deleted,update} = require("../controllers/comments")
-
-
+const schema = require("../schemas/comments")
+const validator = require("../middlewares/validator")
 const passport = require('../config/passport')
 router.route('/comments').get(read)
 
-router.route('/comments').post(passport.authenticate('jwt', {session: false}),create)
-router.route('/comments/:id').put(update)
+router.route('/comments').post(validator(schema),passport.authenticate('jwt', {session: false}),create)
+router.route('/comments/:id').put(passport.authenticate('jwt', {session: false}),update)
 router.route('/comments/:id').delete(passport.authenticate('jwt', {session: false}),deleted)
 
 
