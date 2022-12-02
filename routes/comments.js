25d465013ea6d6bca3fb4ea6eadad75e50f4b141
GinoValenti <ginorvalenti@gmail.com>
2022-12-01 23:41:54 -0300

    a

--- routes/comments.js
+++ routes/comments.js
@@ -6,10 +6,8 @@ const passport = require('../config/passport')
 const isTheSameUser = require('../middlewares/isTheUser')
 const Comment = require("../models/Comment");
 router.route('/comments').get(read)
-
 router.route('/comments').post(validator(schema),passport.authenticate('jwt', {session: false}),create)
 router.route('/comments/:id').put(passport.authenticate('jwt', {session: false}),isTheSameUser(Comment),update)
 router.route('/comments/:id').delete(passport.authenticate('jwt', {session: false}),isTheSameUser(Comment), deleted)
-
 
 module.exports = router
