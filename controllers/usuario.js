203ec5c8e22a68fdcd9afc83077d7bc6f185a80e
GinoValenti <ginorvalenti@gmail.com>
2022-12-05 10:12:02 -0300

    some changes

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -66,7 +66,7 @@ const controller = {
             if (user) {
                 user.verified = true
                 await user.save()
-                res.redirect('http://localhost:3000')
+                res.redirect('https://mytinerary-netrunners.vercel.app/')
             }
             return userNotFoundResponse(req, res)
         } catch (error) {
